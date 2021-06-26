86ec816fadcf095270a31ec27bb20872159b19cc
jahnavi <s.jahnavi1998@gmail.com>
2021-06-26 10:39:18 +0530

    added cart

--- APIS/middlewares/verifyToken.js
+++ APIS/middlewares/verifyToken.js
@@ -3,7 +3,7 @@ const jwt=require("jsonwebtoken")
 const checkToken=(req,res,next)=>{
     try{
         let token=req.headers.authorization.split(" ")[1]
-        jwt.verify(token,'')
+        jwt.verify(token,process.env.SECRET)
         next()
     }
     catch(err){
